fbf990e0d302a4fe7e4f0c0b255e7eabe2aa146f
franceneee <57038559+franceneee@users.noreply.github.com>
2022-05-23 10:02:10 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import hangman from "./hangman.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={hangman} className="App-logo" alt="logo" />
+        <h1>team 15's HANGMAN</h1>
       </header>
+      <div id="gameContainer">
+      </div>
     </div>
   );
 }
