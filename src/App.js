81b92b46331f27009032f72819067a6587b34323
franceneee <57038559+franceneee@users.noreply.github.com>
2022-05-23 10:12:15 +0800

    Add rough guide

--- src/App.js
+++ src/App.js
@@ -9,6 +9,21 @@ function App() {
         <h1>team 15's HANGMAN</h1>
       </header>
       <div id="gameContainer">
+        <div id="container">
+          <p>Lives left</p>
+        </div>
+        <div id="container">
+          <p>hangman drawing, where the animation is supposed to be</p>
+        </div>
+        <div id="container">
+          <p>this is where we have what the user has correctly guessed so far</p>
+        </div>
+        <div id="container">
+          <p>this is where we have the incorrectly guessed letters</p>
+        </div>
+        <div id="container">
+          <p>buttons for the alphabets</p>
+        </div>
       </div>
     </div>
   );
