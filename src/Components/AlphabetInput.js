b4018af8539ba567373b54cfe51d02d18d17f609
franceneee <57038559+franceneee@users.noreply.github.com>
2022-05-26 11:37:17 +0800

    Update AlphabetInput.js

--- src/Components/AlphabetInput.js
+++ src/Components/AlphabetInput.js
@@ -1,82 +1,82 @@
 export const AlphabetInput = ({ setLetter }) => (
   <>
-    <button value="A" onClick={(event) => setLetter(event.target.value)}>
+    <button value="A" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       A
     </button>
-    <button value="B" onClick={(event) => setLetter(event.target.value)}>
+    <button value="B" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       B
     </button>
-    <button value="C" onClick={(event) => setLetter(event.target.value)}>
+    <button value="C" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       C
     </button>
-    <button value="D" onClick={(event) => setLetter(event.target.value)}>
+    <button value="D" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       D
     </button>
-    <button value="E" onClick={(event) => setLetter(event.target.value)}>
+    <button value="E" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       E
     </button>
-    <button value="F" onClick={(event) => setLetter(event.target.value)}>
+    <button value="F" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       F
     </button>
-    <button value="G" onClick={(event) => setLetter(event.target.value)}>
+    <button value="G" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       G
     </button>
-    <button value="H" onClick={(event) => setLetter(event.target.value)}>
+    <button value="H" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       H
     </button>
-    <button value="I" onClick={(event) => setLetter(event.target.value)}>
+    <button value="I" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       I
     </button>
-    <button value="J" onClick={(event) => setLetter(event.target.value)}>
+    <button value="J" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       J
     </button>
-    <button value="K" onClick={(event) => setLetter(event.target.value)}>
+    <button value="K" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       K
     </button>
-    <button value="L" onClick={(event) => setLetter(event.target.value)}>
+    <button value="L" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       L
     </button>
-    <button value="M" onClick={(event) => setLetter(event.target.value)}>
+    <button value="M" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       M
     </button>
-    <button value="N" onClick={(event) => setLetter(event.target.value)}>
+    <button value="N" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       N
     </button>
-    <button value="O" onClick={(event) => setLetter(event.target.value)}>
+    <button value="O" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       O
     </button>
-    <button value="P" onClick={(event) => setLetter(event.target.value)}>
+    <button value="P" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       P
     </button>
-    <button value="Q" onClick={(event) => setLetter(event.target.value)}>
+    <button value="Q" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       Q
     </button>
-    <button value="R" onClick={(event) => setLetter(event.target.value)}>
+    <button value="R" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       R
     </button>
-    <button value="S" onClick={(event) => setLetter(event.target.value)}>
+    <button value="S" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       S
     </button>
-    <button value="T" onClick={(event) => setLetter(event.target.value)}>
+    <button value="T" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       T
     </button>
-    <button value="U" onClick={(event) => setLetter(event.target.value)}>
+    <button value="U" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       U
     </button>
-    <button value="V" onClick={(event) => setLetter(event.target.value)}>
+    <button value="V" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       V
     </button>
-    <button value="W" onClick={(event) => setLetter(event.target.value)}>
+    <button value="W" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       W
     </button>
 
-    <button value="X" onClick={(event) => setLetter(event.target.value)}>
+    <button value="X" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       X
     </button>
-    <button value="Y" onClick={(event) => setLetter(event.target.value)}>
+    <button value="Y" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       Y
     </button>
-    <button value="Z" onClick={(event) => setLetter(event.target.value)}>
+    <button value="Z" onClick={(event) => setLetter(event.target.value.toLowerCase())}>
       Z
     </button>
   </>
