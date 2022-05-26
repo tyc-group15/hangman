cfad257821f64185ea530aba72a7098477a6f640
franceneee <57038559+franceneee@users.noreply.github.com>
2022-05-26 18:05:06 +0800

    Fix bug in Word.js
    space taken as letter

--- src/Components/Word.js
+++ src/Components/Word.js
@@ -4,7 +4,13 @@ export const Word = ({ answer, correctLetters }) => {
       {answer.split("").map((letter, i) => {
         return (
           <span className="letter" key={i}>
-            {correctLetters.includes(letter) ? <p>&nbsp;{letter}&nbsp;</p>: <p>&nbsp; ____ &nbsp;</p>}
+            {correctLetters.includes(letter) ? (
+              <p>&nbsp;&nbsp;{letter}&nbsp;&nbsp;</p>
+            ) : letter === " " ? (
+              <p>&nbsp;&nbsp;&nbsp;&nbsp;</p>
+            ) : (
+              <p>&nbsp; ____ &nbsp;</p>
+            )}
           </span>
         );
       })}
